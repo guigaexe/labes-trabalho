023f626fce1131d40838d859130e3e4171a1ad84
guigaexe <guilhermeeric@outlook.com>
2018-09-18 17:48:01 -0300

    Criada interface DAO para o Usuario.

--- src/java/modelo/dao/api/UsuarioDAO.java
+++ src/java/modelo/dao/api/UsuarioDAO.java
@@ -1,5 +1,11 @@
 package modelo.dao.api;
 
-public class UsuarioDAO {
+import java.util.List;
+import modelo.Usuario;
+
+public interface UsuarioDAO {
     
+    public Usuario inserir(Usuario usuario);
+    public Usuario encontrarApelido(String apelido);
+    public List<Usuario> encontrarTodos();
 }
