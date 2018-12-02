cadafee386f0c41e66e0f5d5d206901132fb6857
guigaexe <guilhermeeric@outlook.com>
2018-12-02 16:38:56 -0200

    Adicionada media queries para esconder corpo do post em celulares e exemplo de js closure.

--- src/main/webapp/scripts/mudaLogin.js
+++ src/main/webapp/scripts/mudaLogin.js
@@ -1,27 +1,37 @@
 let trocou = false;
 let criou = false;
 
+function trocaBoolean(){
+	let trocou_local = trocou;
+	const closure = function(){
+		trocou_local = !trocou_local;
+		return trocou_local
+	}
+	return closure;
+}
+
 function mudaAtriz(){
 	let titulo = document.getElementsByTagName("H1");
 	let subtitulo = document.getElementsByTagName("H2");
+	let trocar = trocaBoolean()
 	console.log(trocou);
 	if(!trocou){
 		titulo[0].innerHTML = "Sandra Bullock";
 		subtitulo[0].innerHTML = "Um blog sobre Sandra Bullock";
 		$("header").addClass("generico");
 		$("header > h2").css("color", "black");
-		trocou = true;
+		trocou = trocar();
 	}
 	else{
 		titulo[0].innerHTML = "Julia Roberts";
 		subtitulo[0].innerHTML = "Um blog sobre a Julia Roberts.";
 		$("header").removeClass("generico");
 		$("header > h2").css("color", "#738D9C");
-		trocou = false;
+		trocou = trocar();
 	}
 	if(!criou){
 		let span = document.createElement("SPAN");
-		let texto = document.createTextNode("Oi, professor.");
+		let texto = document.createTextNode("Oi, 	professor.");
 		span.appendChild(texto);
 		document.getElementsByTagName("HEADER")[0].appendChild(span);
 		criou = true;
